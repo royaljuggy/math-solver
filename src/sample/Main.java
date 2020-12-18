03a2e259cdf8f1df95c650a05e98d0ae8d5c90bb
royaljuggy <dragonte@hotmail.ca>
2020-12-18 11:03:58 -0500

    Updated LDET solver
    Now handles negative a,b values

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -3,15 +3,21 @@ package sample;
 import javafx.application.Application;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+import java.io.IOException;
+
 public class Main extends Application {
 
     @Override
-    public void start(Stage primaryStage) {
+    public void start(Stage primaryStage) throws IOException  {
+        Parent root2 = FXMLLoader.load(getClass().getResource("sample.fxml"));
+
         primaryStage.setTitle("Hello World!");
         Button btn = new Button();
         btn.setText("Say 'Hello World'");
@@ -21,13 +27,14 @@ public class Main extends Application {
             public void handle(ActionEvent event) {
                 System.out.println("Hello World!");
                 System.out.println(MathFunctions.factorial(4));
-                System.out.println(MathSolvers.linearDiophantine(3, 4, 5));
+                System.out.println(MathSolvers.linearDiophantine(3, -4, -5));
+
             }
         });
 
         StackPane root = new StackPane();
         root.getChildren().add(btn);
-        primaryStage.setScene(new Scene(root, 300, 250));
+        primaryStage.setScene(new Scene(root2, 300, 250));
         primaryStage.show();
     }
 
