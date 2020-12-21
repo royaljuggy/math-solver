14886ba04feaa3621b41c01725890094a8ee291c
royaljuggy <dragonte@hotmail.ca>
2020-12-21 18:13:58 -0500

    Linear Diophantine solver added

--- src/sample/Controller.java
+++ src/sample/Controller.java
@@ -7,8 +7,8 @@ import javafx.scene.text.Text;
 public class Controller {
     @FXML private Text actiontarget;
 
-    @FXML protected void handleSubmitButtonAction(ActionEvent event) {
-        actiontarget.setText(MathSolvers.linearDiophantine(3, -4, -5));
-    }
+    // @FXML protected void handleSubmitButtonAction(ActionEvent event) {
+    //    actiontarget.setText(MathSolvers.linearDiophantine(3, -4, -5));
+    //}
 
 }
